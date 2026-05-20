cc4020cf53d21616a0a72a28ead5ba0aa325910d
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-05-21 04:00:14 +0500

    new maps

--- arknights-tracker/src/lib/data/crafts/craftMaps.js
+++ arknights-tracker/src/lib/data/crafts/craftMaps.js
@@ -1,6 +1,8 @@
 import {machineCrafts} from "$lib/data/crafts/machineCrafts.js";
 import {manualCrafts} from "$lib/data/crafts/manualCrafts.js";
 import {hubCrafts} from "$lib/data/crafts/hubCrafts.js";
+import {miners} from "$lib/data/buildings/miners.js";
+import {pumps} from "$lib/data/buildings/pumps.js";
 
 export const machineCraftItemAsIncome = getCraftItemAs("ingredients", machineCrafts);
 export const machineCraftItemAsOutcome = getCraftItemAs("outcomes", machineCrafts);
@@ -11,6 +13,9 @@ export const manualCraftItemAsOutcome = getCraftItemAs("outcomes", manualCrafts)
 export const hubCraftItemAsIncome = getCraftItemAs("ingredients", hubCrafts);
 export const hubCraftItemAsOutcome = getCraftItemAs("outcomes", hubCrafts);
 
+export const miningItemId2MinerId = getMiningItemId2MinerId();
+
+export const pumpingItemId2PumpId = getPumpingItemId2PumpId();
 
 function getCraftItemAs(fieldName, table) {
     let map = {};
@@ -24,6 +29,40 @@ function getCraftItemAs(fieldName, table) {
             if (!map.hasOwnProperty(itemId)) map[itemId] = [];
 
             map[itemId].push(formulaId);
+        }
+    }
+
+    return map;
+}
+
+function getMiningItemId2MinerId() {
+    let map = {};
+
+    for (let obj of Object.values(miners)) {
+        let minerId = obj.id;
+
+        for (let mineable of Object.values(obj.mineable)) {
+            let itemId = mineable.miningItemId;
+
+            if (!map.hasOwnProperty(itemId)) map[itemId] = [];
+
+            map[itemId].push(minerId);
+        }
+    }
+
+    return map;
+}
+
+function getPumpingItemId2PumpId() {
+    let map = {};
+
+    for (let obj of Object.values(pumps)) {
+        let pumpId = obj.id;
+
+        for (let itemId of obj.enableLiquidIds) {
+            if (!map.hasOwnProperty(itemId)) map[itemId] = [];
+
+            map[itemId].push(pumpId);
         }
     }
 
