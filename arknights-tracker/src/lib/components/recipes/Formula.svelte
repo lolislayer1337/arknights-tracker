50e96e4e91272d773d2e596089236995bcd060a7
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-05-29 00:51:47 +0500

    Formula.svelte

--- arknights-tracker/src/lib/components/recipes/Formula.svelte
+++ arknights-tracker/src/lib/components/recipes/Formula.svelte
@@ -1,23 +1,10 @@
 <script>
-    import {MachineCraft} from "$lib/classes/crafts/MachineCraft.js";
-    import {ManualCraft} from "$lib/classes/crafts/ManualCraft.js";
-    import {HubCraft} from "$lib/classes/crafts/HubCraft.js";
-    import {MiningFormula} from "$lib/classes/crafts/MiningFormula.js";
-    import {PumpingFormula} from "$lib/classes/crafts/PumpingFormula.js";
     import ItemStackCard from "$lib/components/ItemStackCard.svelte";
     import ResourcePointCard from "$lib/components/recipes/ResourcePointCard.svelte";
     import Icons from "$lib/components/Icons.svelte";
 
     export let formula;
     export let mode = ""; // "machineCraft" | "manualCraft" | "hubCraft" | "miningFormula" | "pumpingFormula"
-
-    // $: mode = formula instanceof MachineCraft ? "machineCraft"
-    //     : formula instanceof ManualCraft ? "manualCraft"
-    //     : formula instanceof HubCraft ? "hubCraft"
-    //     : formula instanceof MiningFormula ? "miningFormula"
-    //     : formula instanceof PumpingFormula ? "pumpingFormula"
-    //     : "";
-
     let ingredients;
     let outcomes;
     let craftTimeMs;
