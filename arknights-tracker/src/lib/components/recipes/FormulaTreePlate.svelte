d0b56a7ce93684922204e6c78a61ca1705c251e2
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-04 00:54:51 +0500

    formula tree plate

---
+++ arknights-tracker/src/lib/components/recipes/FormulaTreePlate.svelte
@@ -0,0 +1,8 @@
+<script>
+
+
+</script>
+
+<div class="relative h-full w-full bg-gray-300 dark:bg-[#252525] rounded-3xl border border-gray-200 dark:border-[#444] transition-colors">
+
+</div>
--- arknights-tracker/src/routes/recipes/tree/+page.svelte
+++ arknights-tracker/src/routes/recipes/tree/+page.svelte
@@ -3,6 +3,7 @@
 import Button from "$lib/components/Button.svelte";
 import BottomSheet from "$lib/components/BottomSheet.svelte";
 import FormulaSidebar from "$lib/components/recipes/FormulaSidebar.svelte";
+import FormulaTreePlate from "$lib/components/recipes/FormulaTreePlate.svelte";
 
 let isBottomSheetOpen = false;
 let selectedItemId = "item_plant_grass_1";
@@ -10,11 +11,11 @@ let sidebarMode = "tree"
 
 </script>
 
-<div class="max-w-[100%] max-h-[100%] min-h-screen h-full flex flex-col xl:flex-row">
+<div class="max-w-[100%] flex flex-col xl:flex-row">
 
     <div class="w-full xl:w-[calc(100%-max(470px,30%))] mr-6">
 
-        <div class="flex flex-row gap-6 items-center mb-8">
+        <div class="flex flex-row gap-6 items-center mb-8 h-12">
 
             <Button variant="roundSmall" color="white" onClick={() => history.back()}>
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
@@ -23,12 +24,16 @@ let sidebarMode = "tree"
             </Button>
 
             <div class="flex items-baseline flex-wrap gap-2 md:gap-3 font-sdk">
-
                 <h2 class="text-3xl md:text-5xl tracking-wide text-[#21272C] dark:text-[#FDFDFD]">
                     Recipe tree
                 </h2>
-
             </div>
+
+        </div>
+
+        <div class="w-full h-[calc(100vh-144px)]">
+
+            <FormulaTreePlate />
 
         </div>
 
