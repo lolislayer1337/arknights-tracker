5bb8b6aaaed3fd501d580ed64be1445b8da3914f
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-18 02:37:58 +0500

    add SelectableParamList

---
+++ arknights-tracker/src/lib/components/dataToolbarV2/filterDropdowns/SelectableParamList.svelte
@@ -0,0 +1,68 @@
+<script>
+    export let paramList = [];
+
+    export let getLocaleFunc = (param) => param;
+    export let paramBox;
+
+    // bindable
+    export let selectedParamSet = new Set();
+
+    $: if (!selectedParamSet) {
+        selectedParamSet = new Set();
+    }
+
+    function forceSelectedParamsUpdate() {
+        selectedParamSet = selectedParamSet;
+    }
+
+    function toggleParam(param) {
+        let wasDeleted = selectedParamSet.delete(param);
+
+        if (!wasDeleted) {
+            selectedParamSet.add(param);
+        }
+
+        forceSelectedParamsUpdate();
+    }
+
+    $: isManualMode = selectedParamSet.size === 0;
+
+    $: isParamSelected = (param) => isManualMode && selectedParamSet.has(param);
+
+    $: getBoxStyleMode = (param) => {
+        if (!isManualMode) {
+            return "default";
+        }
+
+        if (isParamSelected(param)) {
+            return "active";
+        }
+
+        return "inactive"
+    }
+</script>
+
+<div class="flex flex-wrap gap-2">
+
+    {#each paramList as param (param)}
+
+        <button
+            class="rounded"
+            on:click={() => toggleParam(param)}
+        >
+
+            <svelte:component
+                this={paramBox}
+                styleMode={getBoxStyleMode(param)}
+                paramId={param}
+            >
+
+                {getLocaleFunc(param)}
+
+            </svelte:component>
+
+        </button>
+
+    {/each}
+
+</div>
