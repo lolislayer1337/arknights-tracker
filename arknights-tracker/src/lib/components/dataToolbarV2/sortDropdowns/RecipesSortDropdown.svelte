8ca457dd56433fb7ad70962a12cba2ac272caa5d
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-17 22:54:38 +0500

    add DraggableParamList

--- arknights-tracker/src/lib/components/dataToolbarV2/sortDropdowns/RecipesSortDropdown.svelte
+++ arknights-tracker/src/lib/components/dataToolbarV2/sortDropdowns/RecipesSortDropdown.svelte
@@ -1,12 +1,13 @@
 <script>
-    import { DraggableList } from "$lib/classes/dragndrop/DraggableList.js";
     import { FactoryEvent } from "$lib/classes/events/FactoryEvent.js";
     import DropdownTemplate from "$lib/components/dataToolbarV2/DropdownTemplate.svelte";
+    import RarityParamBox from "$lib/components/dataToolbarV2/paramBoxes/RarityParamBox.svelte";
+    import TextParamBox from "$lib/components/dataToolbarV2/paramBoxes/TextParamBox.svelte";
+    import DraggableParamList from "$lib/components/dataToolbarV2/sortDropdowns/DraggableParamList.svelte";
     import DraggableSortGroups from "$lib/components/dataToolbarV2/sortDropdowns/DraggableSortGroups.svelte";
     import Icon from "$lib/components/Icon.svelte";
     import { t } from "$lib/i18n";
     import { getDefaultItemSortParams } from "$lib/stores/filterStore.js";
-    import { flip } from "svelte/animate";
 
     export let sortParams = {};
 
@@ -77,29 +78,11 @@
 
         {#if openedSortField === "rarity"}
 
-            <div class="flex flex-wrap gap-2 transition-transform">
-
-                {#each sortParams.sortFieldParams[openedSortField] as filterName}
-
-                    <div
-                        class="h-[32px] px-3 rounded flex items-center gap-1 border transition-all cursor-grab active:cursor-grabbing bg-gray-300 border-gray-400 text-black dark:text-[#E0E0E0] dark:bg-[#424242] dark:border-[#444444] hover:bg-gray-200 hover:dark:bg-[#4a4a4a]"
-                        role="listitem"
-                    >
-
-                        <span class="capitalize font-bold pointer-events-none">
-                            {filterName}
-                        </span>
-
-                        <Icon
-                            name="star"
-                            class="w-3 h-3 text-current pointer-events-none"
-                        />
-
-                    </div>
-
-                {/each}
-
-            </div>
+            <DraggableParamList
+                paramBox={RarityParamBox}
+                getLocaleFunc={(rarity) => rarity}
+                bind:paramList={sortParams.sortFieldParams.rarity}
+            />
 
         {:else if openedSortField === "localeName"}
 
@@ -130,21 +113,11 @@
 
         {:else}
 
-            <div class="flex flex-wrap gap-2">
-
-                {#each sortParams.sortFieldParams[openedSortField] as filterName}
-                    <div
-                        class="h-[32px] px-3 rounded flex items-center gap-1 border transition-all cursor-grab active:cursor-grabbing bg-gray-300 border-gray-400 text-black dark:text-[#E0E0E0] dark:bg-[#424242] dark:border-[#444444] hover:bg-gray-200 hover:dark:bg-[#4a4a4a]"
-                    >
-                        <span class="text-xs capitalize font-bold pointer-events-none">
-                            {getFilterNameLocale(openedSortField, filterName)}
-                        </span>
-
-                    </div>
-
-                {/each}
-
-            </div>
+            <DraggableParamList
+                paramBox={TextParamBox}
+                getLocaleFunc={(param) => getFilterNameLocale(openedSortField, param)}
+                bind:paramList={sortParams.sortFieldParams[openedSortField]}
+            />
 
         {/if}
 
