3dc2f954aa12d04e45fc44bb8561bffc53f4a06f
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-17 20:54:47 +0500

    param boxes

---
+++ arknights-tracker/src/lib/components/dataToolbarV2/paramBoxes/RarityParamBox.svelte
@@ -0,0 +1,19 @@
+<script>
+    import ParamBox from "$lib/components/dataToolbarV2/paramBoxes/ParamBox.svelte";
+    import Icon from "$lib/components/Icon.svelte";
+
+    export let styleMode = "default";
+</script>
+
+<ParamBox styleMode={styleMode}>
+
+    <span class="font-bold pointer-events-none">
+        <slot />
+    </span>
+
+    <Icon
+        name="star"
+        class="w-3 h-3 text-current pointer-events-none"
+    />
+
+</ParamBox>
---
+++ arknights-tracker/src/lib/components/dataToolbarV2/paramBoxes/TextParamBox.svelte
@@ -0,0 +1,17 @@
+<script>
+    import ParamBox from "$lib/components/dataToolbarV2/paramBoxes/ParamBox.svelte";
+
+    export let styleMode = "default";
+</script>
+
+<ParamBox styleMode={styleMode}>
+
+    <slot name="left"/>
+
+    <span class="text-xs font-bold pointer-events-none">
+        <slot />
+    </span>
+
+    <slot name="right"/>
+
+</ParamBox>
