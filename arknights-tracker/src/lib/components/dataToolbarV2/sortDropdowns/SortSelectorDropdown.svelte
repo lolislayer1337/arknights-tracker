0b502e9f1979944208c5c743a8f70e6fc225d2b7
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-19 00:07:50 +0500

    add SortSelectorDropdown

---
+++ arknights-tracker/src/lib/components/dataToolbarV2/sortDropdowns/SortSelectorDropdown.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { t } from "$lib/i18n";
+
+    export let optionList = [];
+    export let getLocaleFunc = (option) => $t(`sort.${option}`);
+
+    // bindable
+    export let selectedOption;
+
+    function selectOption(option) {
+        selectedOption = option;
+    }
+
+    $: isOptionSelected = (option) => option === selectedOption;
+</script>
+
+<div class="absolute left-0 py-1 z-40 w-full flex flex-col bg-white dark:bg-[#383838] rounded-xl shadow-xl border border-gray-100 dark:border-[#444444] overflow-hidden">
+
+    {#each optionList as option}
+
+        <button
+            class="px-4 py-2.5 text-left text-sm hover:bg-gray-50 hover:dark:bg-[#424242] transition-colors {
+                isOptionSelected(option)
+                    ? 'text-black font-bold bg-gray-50 dark:text-[#E0E0E0] dark:bg-[#424242]'
+                    : 'text-gray-600 dark:text-[#B7B6B3]'
+            }"
+            on:click|stopPropagation={() => selectOption(option)}
+        >
+
+            {getLocaleFunc(option)}
+
+        </button>
+
+    {/each}
+
+</div>
