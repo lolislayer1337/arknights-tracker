a5a4c7dab7e10da43c91ee61642a7ec5112f1372
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-19 03:08:24 +0500

    add GroupTitle

---
+++ arknights-tracker/src/lib/components/dataToolbarV2/GroupTitle.svelte
@@ -0,0 +1,16 @@
+<script>
+    export let asButton = false;
+
+    export let onClick;
+</script>
+
+<svelte:element
+    this={asButton ? 'button' : 'div'}
+    role={asButton ? "button" : "presentation"}
+    class="text-sm dark:text-[#E0E0E0] font-bold text-gray-800 {asButton ? 'hover:opacity-70 cursor-pointer' : ''}"
+    on:click={asButton ? onClick : undefined}
+>
+
+    <slot />
+
+</svelte:element>
