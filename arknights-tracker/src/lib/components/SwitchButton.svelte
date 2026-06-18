273bd081489643ddf599a2620a8633ba1c4aa9ba
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-06-19 02:52:03 +0500

    add SwitchButton

---
+++ arknights-tracker/src/lib/components/SwitchButton.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let isActive = false;
+
+    function toggle() {
+        isActive = !isActive;
+    }
+</script>
+
+<button
+    role="switch"
+    aria-label="switch"
+    aria-checked={isActive}
+    class="relative inline-flex h-6 w-11 items-center rounded-full transition-colors focus:outline-none {
+        isActive
+            ? 'bg-[#F9B90C]'
+            : 'bg-gray-200 dark:bg-[#555]'
+    }"
+    on:click={toggle}
+>
+
+    <span
+        class="inline-block h-4 w-4 transform rounded-full bg-white transition-transform shadow-sm {
+            isActive
+                ? 'translate-x-6'
+                : 'translate-x-1'
+        }"
+    ></span>
+
+</button>
