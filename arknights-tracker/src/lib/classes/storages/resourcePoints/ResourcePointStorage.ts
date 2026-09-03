0aa4d77558b86d05e4f0a19f5a1c6e31cde0405f
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-09-04 01:07:11 +0500

    ResourcePointStorage fix

--- arknights-tracker/src/lib/classes/storages/resourcePoints/ResourcePointStorage.ts
+++ arknights-tracker/src/lib/classes/storages/resourcePoints/ResourcePointStorage.ts
@@ -1,16 +1,34 @@
 import { DataMap } from "$lib/classes/gameData/collections/DataMap";
 import type { IReadonlyDataMap } from "$lib/classes/gameData/collections/IReadonlyDataMap";
 import type { IResourcePoint } from "$lib/classes/gameData/resourcePoints/IResourcePoint";
+import { ResourcePoint } from "$lib/classes/gameData/resourcePoints/ResourcePoint";
 import { GameDataStorage } from "$lib/classes/storages/GameDataStorage";
+import type { IDataStorage } from "$lib/classes/storages/IDataStorage";
+import type { IItemStorage } from "$lib/classes/storages/items/IItemStorage";
 import type { IResourcePointStorage } from "$lib/classes/storages/resourcePoints/IResourcePointStorage";
+import type { ResourcePointData } from "$lib/data/types/items/ResourcePointData";
 
 export class ResourcePointStorage extends GameDataStorage<IResourcePoint> implements IResourcePointStorage {
     private readonly _byItemId: IReadonlyDataMap<string, IResourcePoint>;
 
-    public constructor(list: IResourcePoint[]) {
+    public constructor(dataStorage: IDataStorage<ResourcePointData>, itemStorage: IItemStorage) {
+        const list = ResourcePointStorage.getList(dataStorage, itemStorage);
+
         super(list);
 
         this._byItemId = DataMap.create(list, item => item.item.gameId);
+    }
+
+    private static getList(dataStorage: IDataStorage<ResourcePointData>, itemStorage: IItemStorage): IResourcePoint[] {
+        const result: IResourcePoint[] = [];
+
+        for (const data of dataStorage.list) {
+            const item = itemStorage.byGameId.getOrThrow(data.itemId);
+
+            result.push(ResourcePoint.createFromData(data, item));
+        }
+
+        return result;
     }
 
     public get byItemId(): IReadonlyDataMap<string, IResourcePoint> {
