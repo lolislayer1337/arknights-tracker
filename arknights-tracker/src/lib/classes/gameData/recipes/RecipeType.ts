751c9372fdb9ddefa9c5ce068dc99aa99c580207
smnd <91721976+lolislayer1337@users.noreply.github.com>
2026-09-08 13:47:09 +0500

    RecipeType

---
+++ arknights-tracker/src/lib/classes/gameData/recipes/RecipeType.ts
@@ -0,0 +1,8 @@
+export enum RecipeType {
+    MANUAL = "manualCraft",
+    HUB = "hubCraft",
+    MACHINE = "machineCraft",
+    MINING = "miningFormula",
+    GAS_MINING = "gasMiningFormula",
+    PUMPING = "pumpingFormula"
+}
